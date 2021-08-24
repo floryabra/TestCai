a94e55ce416192565afca740fc150895ae13885e
florencia.yabra <florencia.yabra@avature.net>
2021-08-23 23:06:58 -0300

    testCai

---
+++ TestCai/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestCai
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World, this is a test!");
+        }
+    }
+}
